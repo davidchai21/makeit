c7e7279f668ead7ba1d1eeef693e6548b591f414
Rongdong Chai <rchai@email.wm.edu>
2021-01-04 02:13:16 -0600

    challenge

---
+++ 0021_MergeTwoSortedLists/MergeTwoSortedLists.cc
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 || !l2) return l1 ? l1 : l2;
+        if (l1->val > l2->val) return mergeTwoLists(l2, l1);
+        ListNode* res = l1;
+        ListNode* cur = l1;
+        l1 = l1->next;
+        while (l1 && l2) {
+            if (l1->val > l2->val) {
+                cur->next = l2;
+                l2 = l2->next;
+            } else {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            cur = cur->next;
+        }
+        if (l1) cur->next = l1;
+        if (l2) cur->next = l2;
+        return res;
+    }
+};
+
+// Recursive:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 || !l2) return l1 ? l1 : l2;
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
