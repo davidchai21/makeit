29705c29f48bb2b07cf784100ed05e9b0b92ff1f
Rongdong Chai <rchai@email.wm.edu>
2022-05-18 04:05:29 -0500

    2274

---
+++ 2274_MaximumConsecutiveFloorsWithoutSpecialFloors/MaximumConsecutiveFloorsWithoutSpecialFloors.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        sort(special.begin(), special.end());
+        int res = max(special.front()-bottom, top-special.back());
+        for (int i=1;i<special.size();++i) {
+            res = max(res, special[i]-special[i-1]-1);
+        }
+        return res;
+    }
+};
